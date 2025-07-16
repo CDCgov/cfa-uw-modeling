1c64955a74338865db21d86f102aeb826b7ad189
Emily Pollock <ruu7@cdc.gov>
2025-07-16 22:11:59 +0000

    reorg-ed and updated fit scripts

---
+++ scripts/prep/1_calc_network_targets.R
@@ -0,0 +1,262 @@
+# load data
+w <- readRDS(here::here("data", "nsfg_wide.rds"))
+l <- readRDS(here::here("data", "nsfg_long.rds"))
+
+# Set population parameters ---------------------------
+out <- list()
+out$pop$size <- 50000
+
+out$pop$female$levels <- 0:1
+out$pop$female$dist <- c(0.5, 0.5)
+
+out$pop$age$min <- 15
+out$pop$age$max <- 49
+out$pop$age_group$levels <- 1:7
+out$pop$age_group$dist <- c(rep(0.143, 6), 0.142) # even ages with slightly fewer 45-49
+
+out$pop$race$levels <- c("B", "H", "O", "W")
+out$pop$race$dist <- c(0.12, 0.19, 0.11, 0.58) # based on 2020 census
+
+# Rake surveys to population margins ---------------------------
+pop_agegroup <- data.frame(age_group = out$pop$age_group$levels, Freq = out$pop$age_group$dist * 144764299)
+pop_race <- data.frame(race = out$pop$race$levels, Freq = out$pop$race$dist * 144764299)
+
+wsvy <- srvyr::as_survey_design(w, ids = secu, strata = sest, weights = weight, nest = TRUE)
+lsvy <- srvyr::as_survey_design(l, ids = secu, strata = sest, weights = weight, nest = TRUE)
+
+wsvy <- survey::rake(wsvy, list(~age_group, ~race), list(pop_agegroup, pop_race))
+lsvy <- survey::rake(lsvy, list(~age_group, ~race), list(pop_agegroup, pop_race))
+
+# Add age squared and cross-network ties ---------------------------
+wsvy <- wsvy |> dplyr::mutate(
+  agesq = age^2,
+  agecb = age^3,
+  cross_network = ifelse(deg_main > 0 & deg_casual > 0, 1, 0),
+  conc = ifelse(deg_casual > 1, 1, 0)
+)
+
+lsvy <- lsvy |> dplyr::mutate(
+  absdiff_sqrt_age = abs(sqrt(age) - sqrt(alter_age))
+)
+
+# Main Network Parameters -----------------------------------
+## Edges
+mod <- survey::svyglm(deg_main ~ 1, design = wsvy, family = quasipoisson())
+pred <- exp(coef(mod)[[1]])
+out$main$edges <- pred
+
+## Older partners
+mod <- survey::svyglm(olderpartner ~ age, design = wsvy, family = quasipoisson())
+pred <- predict(mod, newdata = data.frame(age = 15:49), type = "response")
+out$main$olderpartner <- as.numeric(pred)
+
+## Cross-network ties (main-casual)
+mod <- survey::svyglm(cross_network ~ 1, design = wsvy, family = quasipoisson())
+pred <- exp(coef(mod)[[1]])
+out$main$cross_network <- pred
+out$casual$cross_network <- pred
+
+## Nodefactor (activity by age group and race)
+mod <- survey::svyglm(deg_main ~ age_group, design = wsvy, family = quasipoisson())
+pred <- predict(mod, newdata = data.frame(age_group = out$pop$age_group$levels), type = "response")
+out$main$nodefactor$age_group <- as.numeric(pred)
+
+mod <- survey::svyglm(deg_main ~ race, design = wsvy, family = quasipoisson())
+pred <- predict(mod, newdata = data.frame(race = out$pop$race$levels), type = "response")
+out$main$nodefactor$race <- as.numeric(pred)
+
+mod <- survey::svyglm(deg_main ~ age + agesq + race, design = wsvy, family = quasipoisson())
+pred <- predict(mod, newdata = data.frame(
+  age = rep(15:49, 4),
+  agesq = (rep(15:49, 4))**2,
+  race = rep(out$pop$race$levels, each = length(15:49))
+), type = "response")
+out$main$nodefactor$age_race <- as.numeric(pred)
+
+## Nodecov (mean of combined age and agesq across all rels)
+mod <- survey::svyglm(comb_age ~ 1, design = lsvy, subset = rel2 == "Marriage/Cohab", family = quasipoisson())
+pred <- exp(coef(mod)[[1]])
+out$main$nodecov$age <- pred
+
+mod <- survey::svyglm(comb_agesq ~ 1, design = lsvy, subset = rel2 == "Marriage/Cohab", family = quasipoisson())
+pred <- exp(coef(mod)[[1]])
+out$main$nodecov$agesq <- pred
+
+# Nodematch (by race, and overall)
+mod <- survey::svyglm(race_match ~ race, design = lsvy, subset = rel2 == "Marriage/Cohab", family = quasipoisson())
+pred <- predict(mod, newdata = data.frame(race = out$pop$race$levels), type = "response")
+out$main$nodematch$race <- as.numeric(pred)
+
+# Absdiff (mean abs val in sqrt of age between partners)
+mod <- survey::svyglm(absdiff_sqrt_age ~ 1,
+  design = lsvy,
+  subset = rel2 == "Marriage/Cohab", family = quasipoisson()
+)
+pred <- exp(coef(mod)[[1]])
+out$main$absdiff_sqrt_age <- pred
+
+# Casual Relationships Parameters -------------------
+## Edges
+mod <- survey::svyglm(deg_casual ~ 1, design = wsvy, family = quasipoisson())
+pred <- exp(coef(mod)[[1]])
+out$casual$edges <- pred
+
+## Concurrency
+mod <- survey::svyglm(conc ~ 1, design = wsvy, family = quasipoisson())
+pred <- exp(coef(mod)[[1]])
+out$casual$concurrent <- pred
+
+## Nodefactor (activity by age and race)
+mod <- survey::svyglm(deg_casual ~ age + agesq + agecb + race, design = wsvy, family = quasipoisson())
+pred <- predict(mod, newdata = data.frame(
+  age = rep(15:49, 4),
+  agesq = (rep(15:49, 4))**2,
+  agecb = (rep(15:49, 4))**3,
+  race = rep(out$pop$race$levels, each = length(15:49))
+), type = "response")
+out$casual$nodefactor$age_race <- as.numeric(pred)
+
+## Nodematch (by race)
+mod <- survey::svyglm(race_match ~ race, design = lsvy, subset = rel2 == "Casual/Other", family = quasipoisson())
+pred <- predict(mod, newdata = data.frame(race = out$pop$race$levels), type = "response")
+out$casual$nodematch$race <- as.numeric(pred)
+
+mod <- survey::svyglm(agegrp_match ~ age_group, design = lsvy, subset = rel2 == "Casual/Other", family = quasipoisson())
+pred <- predict(mod, newdata = data.frame(age_group = out$pop$age_group$levels), type = "response")
+out$casual$nodematch$age_group <- as.numeric(pred)
+
+## Absdiff (mean abs val in sqrt of age between partners)
+mod <- survey::svyglm(absdiff_sqrt_age ~ 1,
+  design = lsvy,
+  subset = rel2 == "Casual/Other" & curr == 1, family = quasipoisson()
+)
+pred <- exp(coef(mod)[[1]])
+out$casual$absdiff_sqrt_age <- pred
+
+## Nodecov (mean of combined age and agesq across all rels)
+mod <- survey::svyglm(comb_age ~ 1, design = lsvy, subset = rel2 == "Casual/Other" & curr == 1, family = quasipoisson())
+pred <- exp(coef(mod)[[1]])
+out$casual$nodecov$age <- pred
+
+mod <- survey::svyglm(comb_agesq ~ 1,
+  design = lsvy, subset = rel2 == "Casual/Other" & curr == 1,
+  family = quasipoisson()
+)
+pred <- exp(coef(mod)[[1]])
+out$casual$nodecov$agesq <- pred
+
+# Instantaneous Relationships Parameters -------------------
+out$inst$summary_time <- "year"
+## Edges
+mod <- survey::svyglm(deg_inst_high ~ 1, design = wsvy, family = quasipoisson())
+pred <- exp(coef(mod)[[1]])
+out$inst$edges <- pred
+
+## Nodefactor (activity by age + race)
+mod <- survey::svyglm(deg_inst_high ~ age + race, design = wsvy, family = quasipoisson())
+pred <- predict(mod, newdata = data.frame(
+  age = rep(15:49, 4),
+  race = rep(out$pop$race$levels, each = length(15:49))
+), type = "response")
+out$inst$nodefactor$age_race <- as.numeric(pred)
+
+## Absdiff (mean abs val in sqrt of age between partners)
+## no info in data on age of only inst partners, so use casual network
+out$inst$absdiff_sqrt_age <- out$casual$absdiff_sqrt_age
+
+# Relationship Durations -----------------------------------
+# calc median rel dur from the survey data
+dur <- lsvy |>
+  dplyr::filter(rel2 == "Marriage/Cohab") |>
+  dplyr::summarize(dur = srvyr::survey_median(partdur, na.rm = TRUE)) |>
+  dplyr::pull(dur)
+
+out$main$duration$overall <- dur * (365 / 12)
+out$main$duration$metric <- "days"
+
+dur <- lsvy |>
+  dplyr::filter(rel2 == "Casual/Other" & curr == 1) |>
+  dplyr::summarize(dur = srvyr::survey_median(partdur, na.rm = TRUE)) |>
+  dplyr::pull(dur)
+
+out$casual$duration$overall <- dur * (365 / 12)
+out$casual$duration$metric <- "days"
+
+# Mixing matrices for constraints ##################################
+alter_race_glm <- survey::svyolr(as.factor(alter_race) ~ race + age + rel2,
+  design = lsvy
+)
+lsvy$variables[, c("B", "H", "O", "W")] <- predict(alter_race_glm, newdata = lsvy$variables, type = "probs")
+
+totrels <- lsvy |>
+  dplyr::group_by(rel2, race) |>
+  dplyr::summarize(
+    tot = srvyr::survey_total(vartype = NULL),
+    B = srvyr::survey_mean(B, vartype = NULL),
+    H = srvyr::survey_mean(H, vartype = NULL),
+    O = srvyr::survey_mean(O, vartype = NULL),
+    W = srvyr::survey_mean(W, vartype = NULL)
+  ) |>
+  dplyr::mutate(
+    B = B * tot,
+    H = H * tot,
+    O = O * tot,
+    W = W * tot
+  ) |>
+  dplyr::ungroup() |>
+  dplyr::select(-race, -rel2, -tot)
+
+mainrels <- totrels[1:4, ]
+casrels <- totrels[5:8, ]
+mainprops <- mainrels / sum(mainrels)
+casprops <- casrels / sum(casrels)
+
+devtools::load_all("epimodel-sti")
+main_race_mixmat <- matrix_symmetrical(mainprops)
+casual_race_mixmat <- matrix_symmetrical(casprops)
+
+out$main$mixmat$race <- main_race_mixmat
+out$casual$mixmat$race <- casual_race_mixmat
+
+alter_age_glm <- survey::svyolr(as.factor(alter_age_group) ~ race + age + rel2, design = lsvy)
+lsvy$variables[, paste0("ag", 1:7)] <- predict(alter_age_glm, newdata = lsvy$variables, type = "probs")
+
+atotrels <- lsvy |>
+  dplyr::group_by(rel2, age_group) |>
+  dplyr::summarize(
+    tot = srvyr::survey_total(vartype = NULL),
+    ag1 = srvyr::survey_mean(ag1, vartype = NULL),
+    ag2 = srvyr::survey_mean(ag2, vartype = NULL),
+    ag3 = srvyr::survey_mean(ag3, vartype = NULL),
+    ag4 = srvyr::survey_mean(ag4, vartype = NULL),
+    ag5 = srvyr::survey_mean(ag5, vartype = NULL),
+    ag6 = srvyr::survey_mean(ag6, vartype = NULL),
+    ag7 = srvyr::survey_mean(ag7, vartype = NULL)
+  ) |>
+  dplyr::mutate(
+    ag1 = ag1 * tot,
+    ag2 = ag2 * tot,
+    ag3 = ag3 * tot,
+    ag4 = ag4 * tot,
+    ag5 = ag5 * tot,
+    ag6 = ag6 * tot,
+    ag7 = ag7 * tot
+  ) |>
+  dplyr::ungroup() |>
+  dplyr::select(-age_group, -rel2, -tot)
+
+amainrels <- atotrels[1:7, ]
+acasrels <- atotrels[8:14, ]
+amainprops <- amainrels / sum(amainrels)
+acasprops <- acasrels / sum(acasrels)
+
+main_ag_mixmat <- matrix_symmetrical(amainprops)
+casual_ag_mixmat <- matrix_symmetrical(acasprops)
+
+out$main$mixmat$age_group <- main_ag_mixmat
+out$casual$mixmat$age_group <- casual_ag_mixmat
+
+
+# save out as yaml
+params_name <- paste0("nw_params.yaml")
+yaml::write_yaml(out, here::here("networks", "params", params_name))
---
+++ scripts/prep/2_fit_networks.R
@@ -0,0 +1,204 @@
+library(EpiModel)
+devtools::load_all("epimodel-sti")
+
+# Required objects --------------------------------------------
+this_seed <- 11111
+dept_rate <- (1 / (50 - 15)) * (1 / 365)
+x <- yaml::read_yaml(here::here("networks", "params", "nw_params.yaml"))
+
+# Generate initial network ----------------------------------------
+## generates empty network with nodal attributes reflecting pop specs
+## and initial guess at casual degree distribution to fit main network
+nw <- generate_init_network(
+  x,
+  seed = this_seed,
+  assign_deg_casual = FALSE,
+  assign_deg_main = TRUE,
+  olderpartner = TRUE
+)
+
+# Estimate Networks ----------------------------------------------
+## Shared control settings ---------------------------
+ergm_controls <- control.ergm(
+  seed = this_seed,
+  MCMC.prop = ~sparse,
+  MCMC.interval = 2048 * 10,
+  MCMC.samplesize = 2048 * 10,
+  SA.interval = 2048 * 10,
+  SA.samplesize = 2048 * 10,
+  main.method = "Stochastic-Approximation"
+)
+## Casual ----------------------------------------------------
+### Formation model
+cas_form <- ~ edges +
+  nodematch(~race, diff = TRUE, levels = c(1:2, 4)) +
+  nodefactor(~race, levels = c(1:2, 4)) +
+  nodefactor(~age_group, levels = 1:5) +
+  concurrent() +
+  offset(nodefactor(~deg_main))
+
+### Calc and Compile Targets
+cas_edges <- calc_targets(nw, x, "casual", "edges")
+cas_nf_agegp <- calc_targets(nw, x, "casual", "nodefactor", "age_group")
+cas_nf_race <- calc_targets(nw, x, "casual", "nodefactor", "race")
+cas_nm_race <- calc_targets(nw, x, "casual", "nodematch", "race", diff = TRUE)
+cas_nm_ag <- calc_targets(nw, x, "casual", "nodematch", "age_group", diff = TRUE)
+cas_nc_age <- calc_targets(nw, x, "casual", "nodecov", "age")
+cas_nc_agesq <- calc_targets(nw, x, "casual", "nodecov", "age", attr_squared = TRUE)
+cas_conc <- calc_targets(nw, x, "casual", "concurrent")
+
+cas_targets <- unname(c(
+  cas_edges,
+  cas_nm_race[c(1:2, 4)],
+  cas_nf_race[c(1:2, 4)],
+  cas_nf_agegp[1:5],
+  cas_conc
+))
+
+### Additional Arguments
+cas_offset <- c(-Inf)
+
+cas_diss <- dissolution_coefs(
+  dissolution = ~ offset(edges),
+  duration = x$casual$duration$overall,
+  d.rate = dept_rate
+)
+
+cas_constraints <- ~
+  sparse +
+    blocks(attr = ~female, levels2 = diag(TRUE, 2)) +
+    strat(attr = ~race, pmat = matrix( # should be same as x$casual$mixmat$race
+      c(
+        0.1354340, 0.0435742, 0.0055867, 0.0050679,
+        0.0435742, 0.0970881, 0.0294654, 0.0386104,
+        0.0055867, 0.0294654, 0.0187281, 0.0441261,
+        0.0050679, 0.0386104, 0.0441261, 0.4158885
+      ),
+      byrow = TRUE,
+      nrow = 4, ncol = 4
+    ))
+
+### Fit
+cas_netest <- EpiModel::netest(
+  nw = nw,
+  formation = cas_form,
+  target.stats = cas_targets,
+  coef.form = cas_offset,
+  coef.diss = cas_diss,
+  constraints = cas_constraints,
+  set.control.ergm = ergm_controls
+)
+
+### Set degree attributes -----------------------------------
+nw <- set.vertex.attribute(nw, "deg_casual", get_degree(cas_netest$newnetwork))
+
+## Main ---------------------------------------------------------
+### Formation model
+main_form <- ~ edges +
+  nodematch(~race, diff = TRUE, levels = c(1:2, 4)) +
+  nodefactor(~race, levels = c(1:2, 4)) +
+  nodecov(~age) +
+  nodecov(~agesq) +
+  absdiff(~ sqrt(age)) +
+  offset(nodefactor(~ deg_casual > 0)) +
+  offset(nodefactor(~ olderpartner > 0))
+
+### Calc and Compile Targets
+main_edges <- calc_targets(nw, x, "main", "edges")
+main_nm_race <- calc_targets(nw, x, "main", "nodematch", "race", diff = TRUE)
+main_nf_race <- calc_targets(nw, x, "main", "nodefactor", "race")
+main_nc_age <- calc_targets(nw, x, "main", "nodecov", "age")
+main_nc_agesq <- calc_targets(nw, x, "main", "nodecov", "age", attr_squared = TRUE)
+main_agemix <- calc_targets(nw, x, "main", "absdiff_sqrt_age")
+
+main_targets <- unname(c(
+  main_edges,
+  main_nm_race[c(1:2, 4)],
+  main_nf_race[c(1:2, 4)],
+  main_nc_age,
+  main_nc_agesq,
+  main_agemix
+))
+
+### Additional Arguments
+main_offset <- rep(-Inf, 2)
+
+main_diss <- dissolution_coefs(
+  dissolution = ~ offset(edges),
+  duration = x$main$duration$overall,
+  d.rate = dept_rate * 1.25 # to account for more relationships among older people in main netwrok
+)
+
+main_constraints <- ~
+  bd(maxout = 1) +
+    sparse +
+    blocks(attr = ~female, levels2 = diag(TRUE, 2)) +
+    strat(attr = ~race, pmat = matrix( # should be same as x$main$mixmat$race
+      c(
+        0.057178141, 0.02842713, 0.00417141, 0.003909043,
+        0.028427131, 0.08849135, 0.02961956, 0.039865982,
+        0.004171410, 0.02961956, 0.02019247, 0.049425911,
+        0.003909043, 0.03986598, 0.04942591, 0.523299963
+      ),
+      byrow = TRUE,
+      nrow = 4, ncol = 4
+    ))
+
+### Fit
+main_netest <- EpiModel::netest(
+  nw = nw,
+  formation = main_form,
+  target.stats = main_targets,
+  coef.form = main_offset,
+  coef.diss = main_diss,
+  constraints = main_constraints,
+  set.control.ergm = ergm_controls
+)
+
+### Set degree attributes -----------------------------------
+nw <- set.vertex.attribute(nw, "deg_main", get_degree(main_netest$newnetwork))
+
+## Inst network ----------------------------------------------------
+### Formation model
+inst_form <- ~ edges +
+  nodefactor(~race, levels = c(1:2, 4)) +
+  nodefactor(~age_group, levels = 1:3) +
+  absdiff(~ sqrt(age))
+
+### Calc and Compile Targets
+inst_edges <- calc_targets(nw, x, "inst", "edges", inst_correct = TRUE, time_unit = "days")
+inst_nf_race <- calc_targets(nw, x, "inst", "nodefactor", "race", inst_correct = TRUE, time_unit = "days")
+inst_nf_agegrp <- calc_targets(nw, x, "inst", "nodefactor", "age_group",
+  inst_correct = TRUE, time_unit = "days"
+)
+inst_agemix <- calc_targets(nw, x, "inst", "absdiff_sqrt_age", inst_correct = TRUE, time_unit = "days")
+
+inst_targets <- unname(c(
+  inst_edges,
+  inst_nf_race[c(1:2, 4)],
+  inst_nf_agegrp[1:3],
+  inst_agemix
+))
+
+### Additional Arguments
+inst_offset <- c() # no offsets for inst network in formation model
+inst_diss <- dissolution_coefs(dissolution = ~ offset(edges), duration = 1, d.rate = 0)
+inst_constraints <- ~ sparse + blocks(attr = ~female, levels2 = diag(TRUE, 2))
+
+### Fit
+inst_netest <- EpiModel::netest(
+  nw = nw,
+  formation = inst_form,
+  target.stats = inst_targets,
+  coef.form = inst_offset,
+  coef.diss = inst_diss,
+  constraints = inst_constraints,
+  set.control.ergm = ergm_controls
+)
+
+# Save out ----------------------------------------------------------
+est <- list(main_netest, cas_netest, inst_netest)
+if (!dir.exists(here::here("networks", "fits", Sys.Date()))) {
+  dir.create(here::here("networks", "fits", Sys.Date()), recursive = TRUE)
+}
+saveRDS(est, file = here::here("networks", "fits", Sys.Date(), "nw.rds"))
