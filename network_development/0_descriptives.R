b1ecf2fddcdb0aa21f91baadb9662b56cc073795
Emily Pollock <ruu7@AZ1CFA1VSWL-39.ext.cdc.gov>
2026-03-17 09:34:38 -0600

    update network dev scripts

--- network_development/0_descriptives.R
+++ network_development/0_descriptives.R
@@ -1,13 +1,19 @@
 library(tidyverse)
+library(epimodelcfa)
 # load data
 # location of survey data
 input_folder <- file.path(here::here("input", "nsfg"))
 
 w <- readRDS(file.path(input_folder, "nsfg_wide.rds"))
+## change levels to force W to be ref category in glms
+w <- w |>
+  mutate(race = factor(race, levels = c("W", "B", "O", "H")))
 l <- readRDS(file.path(input_folder, "nsfg_long.rds"))
+l <- l |>
+  mutate(race = factor(race, levels = c("W", "B", "O", "H")))
 
 # destination folder for parameter yaml
-param_folder <- here::here("input", "params")
+param_folder <- here::here("local_tests", "input", "params")
 if (!dir.exists(param_folder)) {
   dir.create(param_folder, recursive = TRUE)
 }
@@ -22,8 +28,8 @@ out$pop$female$dist <- c(0.5, 0.5)
 out$pop$age$min <- 15
 out$pop$age$max <- 49
 out$pop$age_group$levels <- 1:7
-# even ages with slightly fewer 45-49
-out$pop$age_group$dist <- c(rep(0.143, 6), 0.142)
+# 15-18, 19-24, then 5-yr ints
+out$pop$age_group$dist <- c(0.114, 0.171, rep(0.143, 5))
 
 out$pop$race$levels <- c("B", "H", "O", "W")
 out$pop$race$dist <- c(0.12, 0.19, 0.11, 0.58) # based on 2020 census
@@ -71,16 +77,6 @@ lsvy <- survey::rake(
   list(pop_agegroup, pop_race, pop_sex)
 )
 
-ages <- seq(out$pop$age$min, out$pop$age$max)
-races <- out$pop$race$levels
-females <- out$pop$female$levels
-
-prediction_pop <- tibble(
-  age = rep(rep(ages, each = length(races)), times = length(females)),
-  agesq = age^2,
-  race = rep(rep(races, times = length(ages)), times = length(females)),
-  female = rep(females, each = length(ages) * length(races))
-)
 
 # Add age squared and cross-network ties ---------------------------
 wsvy <- wsvy |>
@@ -96,81 +92,351 @@ lsvy <- lsvy |>
     absdiff_sqrt_age = abs(sqrt(age) - sqrt(alter_age))
   )
 
-# Fit GLMs for main, casual, inst degree ---------------------------
-main_glm <- survey::svyglm(
-  deg_main ~ age + agesq + race,
+# Make Prediction Pop DF for degree- related GLMs ------------------
+ages <- seq(out$pop$age$min, (out$pop$age$max))
+races <- out$pop$race$levels
+
+prediction_pop <- tibble(
+  age = rep(ages, each = length(races)),
+  agesq = age^2,
+  race = rep(races, times = length(ages)),
+  age_group = case_when(
+    age < 50 & age >= 45 ~ 7,
+    age < 45 & age >= 40 ~ 6,
+    age < 40 & age >= 35 ~ 5,
+    age < 35 & age >= 30 ~ 4,
+    age < 30 & age >= 25 ~ 3,
+    age < 25 & age >= 19 ~ 2,
+    age < 19 ~ 1
+  )
+)
+
+out$prediction_pop$age <- prediction_pop$age
+out$prediction_pop$agesq <- prediction_pop$agesq
+out$prediction_pop$age_group <- prediction_pop$age_group
+out$prediction_pop$race <- prediction_pop$race
+
+# -------------------------------------------------------------------------
+# Fit GLMs for main, casual, inst degree EDGES
+# -------------------------------------------------------------------------
+m_edges_mod <- survey::svyglm(
+  deg_main ~ 1,
   design = wsvy,
   family = quasipoisson()
 )
 
-casual_glm <- survey::svyglm(
-  deg_casual ~ I(age) + race + female,
+c_edges_mod <- survey::svyglm(
+  deg_casual ~ 1,
   design = wsvy,
   family = quasipoisson()
 )
 
-inst_glm <- survey::svyglm(
-  deg_inst_high ~ age + agesq + race + female,
+i_edges_mod <- survey::svyglm(
+  deg_inst_high ~ 1,
   design = wsvy,
   family = quasipoisson()
 )
 
-casual_conc_glm <- survey::svyglm(
-  conc ~ age + agesq + race + female,
+out$main$edges <- exp(coef(m_edges_mod)[[1]])
+out$casual$edges <- exp(coef(c_edges_mod)[[1]])
+out$inst$edges <- exp(coef(i_edges_mod)[[1]])
+
+
+## Older partners
+op_glm <- survey::svyglm(
+  olderpartner ~ age + race,
   design = wsvy,
   family = quasibinomial()
 )
 
-observed <- wsvy |>
-  group_by(age, agesq, age_group, race, female) |>
-  reframe(
-    deg_main = srvyr::survey_mean(deg_main, na.rm = TRUE),
-    deg_casual = srvyr::survey_mean(deg_casual, na.rm = TRUE),
-    deg_inst = srvyr::survey_mean(deg_inst_high, na.rm = TRUE),
-    conc = srvyr::survey_mean(conc, na.rm = TRUE),
-  )
+prediction_pop$olderpartner <- predict(
+  op_glm,
+  newdata = prediction_pop,
+  type = "response"
+) #nolint
+out$main$olderpartner <- prediction_pop$olderpartner |> as.numeric()
 
-prediction_pop <- observed |>
-  select(age, agesq, age_group, race, female)
+# -----------------------------------------------------------------------------
+# Nodecov (mean of combined age and agesq across all rels)
+# -----------------------------------------------------------------------------
+## MAIN
+mod <- survey::svyglm(
+  comb_age ~ 1,
+  design = lsvy,
+  subset = rel2 == "Marriage/Cohab",
+  family = quasipoisson()
+)
+pred <- exp(coef(mod)[[1]])
+out$main$nodecov$age <- pred
 
-prediction_pop$deg_main <- predict(main_glm, newdata = prediction_pop, type = "response") #nolint
-prediction_pop$deg_casual <- predict(casual_glm, newdata = prediction_pop, type = "response") #nolint
-prediction_pop$deg_inst <- predict(inst_glm, newdata = prediction_pop, type = "response") #nolint
-prediction_pop$conc <- predict(casual_conc_glm, newdata = prediction_pop, type = "response") #nolint
+mod <- survey::svyglm(
+  comb_agesq ~ 1,
+  design = lsvy,
+  subset = rel2 == "Marriage/Cohab",
+  family = quasipoisson()
+)
+pred <- exp(coef(mod)[[1]])
+out$main$nodecov$agesq <- pred
 
-observed |>
-  ggplot(aes(x = age, y = deg_casual)) +
-  facet_wrap(~race + female) +
-  geom_point() +
-  geom_smooth() +
-  geom_line(data = prediction_pop, aes(x = age, y = deg_casual), color = "red")
+## CASUAL
+mod <- survey::svyglm(
+  comb_age ~ 1,
+  design = lsvy,
+  subset = rel2 == "Casual/Other",
+  family = quasipoisson()
+)
+pred <- exp(coef(mod)[[1]])
+out$casual$nodecov$age <- pred
 
-observed |>
-  ggplot(aes(x = age, y = deg_main)) +
-  facet_wrap(~race + female) +
-  geom_point() +
-  geom_smooth() +
-  geom_line(data = prediction_pop, aes(x = age, y = deg_main), color = "red")
+mod <- survey::svyglm(
+  comb_agesq ~ 1,
+  design = lsvy,
+  subset = rel2 == "Casual/Other",
+  family = quasipoisson()
+)
 
-observed |>
-  ggplot(aes(x = age, y = deg_inst)) +
-  facet_wrap(~race + female) +
-  geom_point() +
-  geom_smooth() +
-  geom_line(data = prediction_pop, aes(x = age, y = deg_inst), color = "red")
+pred <- exp(coef(mod)[[1]])
+out$casual$nodecov$agesq <- pred
 
-observed |>
-  ggplot(aes(x = age, y = conc)) +
-  facet_wrap(~race + female) +
-  geom_point() +
-  geom_smooth() +
-  geom_line(data = prediction_pop, aes(x = age, y = conc), color = "red")
+# -----------------------------------------------------------------------------
+# Nodematch (Age Group and Race, global and per-group)
+# -----------------------------------------------------------------------------
+## Age group - by group
+agegrp_glm_each <- survey::svyglm(
+  agegrp_match ~ rel2 + as.factor(age_group),
+  design = lsvy
+)
 
-prediction_pop |>
-  group_by(race) |>
-  summarize(mean_casual_conc = mean(conc))
+agegrp_data <- tibble(
+  rel2 = rep(c("Marriage/Cohab", "Casual/Other"), each = 7),
+  age_group = rep(as.factor(1:7), 2)
+)
 
-lsvy |>
-  group_by(rel) |>
+agegrp_data$prob <- predict(
+  agegrp_glm_each,
+  newdata = agegrp_data,
+  type = "response"
+)
 
-  summarize(mean_absdiff = srvyr::survey_mean(absdiff_sqrt_age, na.rm = TRUE))
+out$main$nodematch$age_group$each <- agegrp_data |>
+  filter(rel2 == "Marriage/Cohab") |>
+  pull(prob) |>
+  as.numeric()
+
+out$casual$nodematch$age_group$each <- agegrp_data |>
+  filter(rel2 == "Casual/Other") |>
+  pull(prob) |>
+  as.numeric()
+
+## Age group - global
+agegrp_glm <- survey::svyglm(
+  agegrp_match ~ rel2,
+  design = lsvy
+)
+rel_cats <- tibble(rel2 = c("Marriage/Cohab", "Casual/Other"))
+rel_cats$ag_match <- predict(agegrp_glm, newdata = rel_cats, type = "response")
+
+out$main$nodematch$age_group$global <- rel_cats |>
+  filter(rel2 == "Marriage/Cohab") |>
+  pull(ag_match) |>
+  as.numeric()
+
+out$casual$nodematch$age_group$global <- rel_cats |>
+  filter(rel2 == "Casual/Other") |>
+  pull(ag_match) |>
+  as.numeric()
+
+## Race - by group
+race_glm_each <- survey::svyglm(
+  race_match ~ rel2 + as.factor(race),
+  design = lsvy
+)
+
+race_data <- tibble(
+  rel2 = rep(c("Marriage/Cohab", "Casual/Other"), each = 4),
+  race = rep(c("B", "H", "O", "W"), 2)
+)
+
+race_data$prob <- predict(
+  race_glm_each,
+  newdata = race_data,
+  type = "response"
+)
+
+out$main$nodematch$race$each <- race_data |>
+  filter(rel2 == "Marriage/Cohab") |>
+  pull(prob) |>
+  as.numeric()
+
+out$casual$nodematch$race$each <- race_data |>
+  filter(rel2 == "Casual/Other") |>
+  pull(prob) |>
+  as.numeric()
+
+## Race - global
+race_glm <- survey::svyglm(
+  race_match ~ rel2,
+  design = lsvy
+)
+rel_cats <- tibble(rel2 = c("Marriage/Cohab", "Casual/Other"))
+rel_cats$race_match <- predict(race_glm, newdata = rel_cats, type = "response")
+
+out$main$nodematch$race$global <- rel_cats |>
+  filter(rel2 == "Marriage/Cohab") |>
+  pull(race_match) |>
+  as.numeric()
+
+out$casual$nodematch$race$global <- rel_cats |>
+  filter(rel2 == "Casual/Other") |>
+  pull(race_match) |>
+  as.numeric()
+
+# -----------------------------------------------------------------
+# Casual Concurrency ----------------------------------------------
+# -----------------------------------------------------------------
+## Concurrency in casual network
+conc_glm <- survey::svyglm(
+  conc ~ age + age_group + race,
+  design = wsvy,
+  family = quasibinomial()
+)
+
+predicted <- predict(conc_glm, newdata = prediction_pop, type = "response")
+out$casual$concurrent <- mean(predicted)
+
+# -----------------------------------------------------------------
+# Absdiff Age ----------------------------------------------
+# -----------------------------------------------------------------
+
+# Absdiff (mean abs val in sqrt of age between partners)
+mod <- survey::svyglm(
+  absdiff_sqrt_age ~ 1,
+  design = lsvy,
+  subset = rel2 == "Marriage/Cohab",
+  family = quasipoisson()
+)
+pred <- exp(coef(mod)[[1]])
+out$main$absdiff_sqrt_age <- pred
+
+
+## Absdiff (mean abs val in sqrt of age between partners)
+mod <- survey::svyglm(
+  absdiff_sqrt_age ~ 1,
+  design = lsvy,
+  subset = rel2 == "Casual/Other" & curr == 1,
+  family = quasipoisson()
+)
+pred <- exp(coef(mod)[[1]])
+out$casual$absdiff_sqrt_age <- pred
+
+## Absdiff (mean abs val in sqrt of age between partners)
+## no info in data on age of only inst partners, so use casual network
+out$inst$absdiff_sqrt_age <- out$casual$absdiff_sqrt_age
+
+# Relationship Durations -----------------------------------
+# calc median rel dur from the survey data
+dur <- lsvy |>
+  dplyr::filter(rel2 == "Marriage/Cohab") |>
+  dplyr::summarize(dur = srvyr::survey_median(partdur, na.rm = TRUE)) |>
+  dplyr::pull(dur)
+
+out$main$duration$overall <- dur * (365 / 12)
+out$main$duration$metric <- "days"
+
+dur <- lsvy |>
+  dplyr::filter(rel2 == "Casual/Other" & curr == 1) |>
+  dplyr::summarize(dur = srvyr::survey_median(partdur, na.rm = TRUE)) |>
+  dplyr::pull(dur)
+
+out$casual$duration$overall <- dur * (365 / 12)
+out$casual$duration$metric <- "days"
+
+# Mixing matrices for constraints ##################################
+alter_race_glm <- survey::svyolr(
+  as.factor(alter_race) ~ race + age + rel2,
+  design = lsvy
+)
+lsvy$variables[, c("B", "H", "O", "W")] <- predict(
+  alter_race_glm,
+  newdata = lsvy$variables,
+  type = "probs"
+)
+
+totrels <- lsvy |>
+  dplyr::group_by(rel2, race) |>
+  dplyr::summarize(
+    tot = srvyr::survey_total(vartype = NULL),
+    B = srvyr::survey_mean(B, vartype = NULL),
+    H = srvyr::survey_mean(H, vartype = NULL),
+    O = srvyr::survey_mean(O, vartype = NULL),
+    W = srvyr::survey_mean(W, vartype = NULL)
+  ) |>
+  dplyr::mutate(
+    B = B * tot,
+    H = H * tot,
+    O = O * tot,
+    W = W * tot
+  ) |>
+  dplyr::ungroup() |>
+  dplyr::select(-race, -rel2, -tot)
+
+mainrels <- totrels[1:4, ]
+casrels <- totrels[5:8, ]
+mainprops <- mainrels / sum(mainrels)
+casprops <- casrels / sum(casrels)
+
+main_race_mixmat <- epimodelcfa::matrix_symmetrical(mainprops)
+casual_race_mixmat <- epimodelcfa::matrix_symmetrical(casprops)
+
+out$main$mixmat$race <- main_race_mixmat
+out$casual$mixmat$race <- casual_race_mixmat
+
+alter_age_glm <- survey::svyolr(
+  as.factor(alter_age_group) ~ race + age + rel2,
+  design = lsvy
+)
+lsvy$variables[, paste0("ag", 1:7)] <- predict(
+  alter_age_glm,
+  newdata = lsvy$variables,
+  type = "probs"
+)
+
+atotrels <- lsvy |>
+  dplyr::group_by(rel2, age_group) |>
+  dplyr::summarize(
+    tot = srvyr::survey_total(vartype = NULL),
+    ag1 = srvyr::survey_mean(ag1, vartype = NULL),
+    ag2 = srvyr::survey_mean(ag2, vartype = NULL),
+    ag3 = srvyr::survey_mean(ag3, vartype = NULL),
+    ag4 = srvyr::survey_mean(ag4, vartype = NULL),
+    ag5 = srvyr::survey_mean(ag5, vartype = NULL),
+    ag6 = srvyr::survey_mean(ag6, vartype = NULL),
+    ag7 = srvyr::survey_mean(ag7, vartype = NULL)
+  ) |>
+  dplyr::mutate(
+    ag1 = ag1 * tot,
+    ag2 = ag2 * tot,
+    ag3 = ag3 * tot,
+    ag4 = ag4 * tot,
+    ag5 = ag5 * tot,
+    ag6 = ag6 * tot,
+    ag7 = ag7 * tot
+  ) |>
+  dplyr::ungroup() |>
+  dplyr::select(-age_group, -rel2, -tot)
+
+amainrels <- atotrels[1:7, ]
+acasrels <- atotrels[8:14, ]
+amainprops <- amainrels / sum(amainrels)
+acasprops <- acasrels / sum(acasrels)
+
+main_ag_mixmat <- matrix_symmetrical(amainprops)
+casual_ag_mixmat <- matrix_symmetrical(acasprops)
+
+out$main$mixmat$age_group <- main_ag_mixmat
+out$casual$mixmat$age_group <- casual_ag_mixmat
+
+
+# save out as yaml
+params_name <- "nw_params.yaml"
+yaml::write_yaml(out, file.path(param_folder, params_name))
--- network_development/3_network_diagnostics.R
+++ network_development/3_network_diagnostics.R
@@ -1,11 +1,22 @@
 library(epimodelcfa)
-folder_name <- "latest"
-nets <- readRDS(here::here("input", "network_fits", folder_name, "nw.rds"))
-yaml_params_loc <- here::here("input", "params", "nw_params.yaml")
+
+nets <- readRDS(here::here(
+  "input",
+  "network_fits",
+  "nw.rds"
+))
+
+yaml_params_loc <- here::here(
+  "input",
+  "params",
+  "nw_params.yaml"
+)
 
 # MCMC Diagnostics & GOF ---------------------------------------------------
-## note: for some reason, the plots look worse when using SA than using MCMLE, but other metrics ok
-## note: GOF metrics will be slightly different every time it is run, but should be similar to each other
+## note: for some reason, the plots look worse when using
+# SA than using MCMLE, but other metrics ok
+## note: GOF metrics will be slightly different every time it is run,
+# but should be similar to each other
 ## note: FYI GOF also takes a while to run
 
 m1 <- mcmc.diagnostics(nets[[1]]$fit)
@@ -20,11 +31,10 @@ m3 <- mcmc.diagnostics(nets[[3]]$fit)
 plot(m3)
 g3 <- gof(nets[[3]]$fit, GOF = ~model)
 
-# Network Diagnostics via Simulation --------------------------------------------------------
-## Shared Parameters
+# Network Diagnostics via Simulation ------------
 
-ncores <- 10
-nsims <- 20
+ncores <- parallel::detectCores()
+nsims <- 10
 nsteps <- 1000
 main_casual_ergm_control <- control.simulate.formula(
   MCMC.prop = ~sparse,
@@ -67,14 +77,20 @@ inst_static <- netdx(nets[[3]], dynamic = FALSE, nsims = 500)
 inst_static
 
 # Plot Degree Distributions --------------------------------------------
-## Note: Because the ERGM terms used as target statistics in the model fit do not discretely represent each
-## age and race degree combination, the degree distributions will not match the empirical distributions perfectly,
+## Note: Because the ERGM terms used as target statistics in the model
+# fit do not discretely represent each
+## age and race degree combination, the degree distributions will not
+#match the empirical distributions perfectly,
 ## but should be similar in shape.
 ##
-## Note 2: The networks within the netest and netdx objects are independent of each other, and the netdx
-## objects are simulated in a closed population. Thus, we will need to run a netsim simulation to see how
-## the degree distributions change over time when simulated with vital dynamics and changing nodal degree terms
+## Note 2: The networks within the netest and netdx objects are independent
+# of each other, and the netdx
+## objects are simulated in a closed population. Thus, we will need to run a
+# netsim simulation to see how
+## the degree distributions change over time when simulated with
+# vital dynamics and changing nodal degree terms
 ## that influence formation in other networks.
+## See `04_network_vital_dynamics_diag.R`
 
 ## Degree based on netest object fit
 plot_final_degrees(nets[[1]], "main", yaml_params_loc)
